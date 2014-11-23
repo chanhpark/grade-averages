174dbca654d99861265fd1b72947a4e811ee342c
chanhpark <chanhyun.park1211@gmail.com>
2014-11-23 14:46:22 -0500

    clinic from launch academy

---
+++ clinic.rb
@@ -0,0 +1,36 @@
+# ###read in information from jason file and create an array of student hashes
+# require 'pry'
+# require 'json'
+#
+# json = File.read('students.json')
+# students = JSON.parse(json)['students']
+#
+# students.each do |student|
+#   total = 0.0
+#
+#   student['grades'].each do |grade|
+#     total += grade
+#
+#   end
+#
+#   average = total / student['grades'].length
+#
+# name = student['name']
+#   max = student['grades'].max
+#   min = studnet['grades'].min
+
+#
+# end
+
+def average_grade(grades,name, teacher)
+  total = 0.0 average =
+end
+)
+
+
+def student_records(filename)
+  json = File.read('filename')
+  JSON.parse(json)['students']
+end
+
+students = student_records('students.json')
