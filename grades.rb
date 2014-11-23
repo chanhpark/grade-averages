79cf74f7a065d97917432ed6e02f082e768cc732
chanhpark <chanhyun.park1211@gmail.com>
2014-11-23 15:47:02 -0500

    readability

--- grades.rb
+++ grades.rb
@@ -7,12 +7,12 @@ final_array = []
 final_list = ''
 
 grades["students"].each do |student|
-  name = (student)["name"]
-  grades_avg = (student["grades"])
-  grades_sort = (student)["grades"].sort.reverse
-  high_grade = grades_sort[0].to_i
-  low_grade = grades_sort[-1].to_i
-  averages = grades_avg.inject { |x, y| x + y }.to_f / grades_avg.size
+  name = student["name"]
+  student_grades = student["grades"]
+  grades_sort = student["grades"].sort.reverse
+  high_grade = grades_sort.first.to_i
+  low_grade = grades_sort.last.to_i
+  averages = student_grades.inject(:+) / student_grades.size.to_f
   final_list  << "#{name}: " +  "#{averages}\t #{high_grade}\t #{low_grade}\n"
 end
 
